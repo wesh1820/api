734ad3f72f8993d9e44467a2f39a3a06641ef6d6
Wesley Roekens <Roekenswesley@gmail.com>
2024-10-04 14:22:53 +0200

    cors

--- app.js
+++ app.js
@@ -6,22 +6,17 @@ var logger = require('morgan');
 
 const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users'); 
-
 var app = express(); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors()); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
 app.use('/api/v1/messages', require('./routes/api/v1/messages'));
 
 // catch 404 and forward to error handler
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -5,27 +5,88 @@ var router = express.Router();
 router.get("/", (req, res, next) => {
     res.status(200).json({ 
         status: "success",
-        message: "GET messages",
-        data: [
-            {
-                id: 1,
-                user: "user1",
-                message: "Hello, this is a test message."
-            },
-            {
-                id: 2,
-                user: "user2",
-                message: "Another test message."
-            },
-            {
-                id: 3,
-                user: "user3",
-                message: "Yet another message."
-            }
-        ]
+        message:  "GET messages",
+        data: {
+            "messages": [
+                {
+                    id: 1,
+                    user: "user1",
+                    message: "message1"
+                },
+                {
+                    id: 2,
+                    user: "user2",
+                    message: "message2"
+                }
+            ]
+        }
     });
 });
+//geef één bericht terug op basis van de id
+//je kan hier de array keys 0, 1, ... gebruiken of gewoon één bericht terugsturen naar keuze
+//als een bericht niet gevonden kan worden kan je status code 404 teruggeven ipv 200
+
+router.get("/:id", (req, res, next) => {
+    res.status(200).json({ 
+        status: "success",
+        message:  "GET message",
+        data: {
+            "message": {
+                id: 1,
+            }
+        }
+    });
+}
+);
+
+//POST: /api/v1/messages
+//deze route voegt één nieuw bericht toe aan je array
+//Body: { message: { “user”: “Pikachu”, “text”: “nodejs isn’t hard, or is it?” } }
+//zorg dat je de naam en text zelf kan invullen via postman
+//je response bevat status en message maar ook .data.message met daarin je nieuw bericht
+
+router.post("/", (req, res, next) => {
+    res.status(200).json({ 
+        status: "success",
+        message:  "POST message",
+        data: {
+            "message": {
+                id: 1,
+                
+            }
+        }
+    });
+}
+);
+
+//PUT: /api/v1/messages/:id
+//hiermee kan je een specifiek bericht updaten (vb: text aanpassen)
+//je response bevat status en message maar ook .data.message met daarin je nieuw bericht
+
+router.put("/:id", (req, res, next) => {
+    res.status(200).json({
+        status: "success",
+        message: "PUT message",
+        data: {
+            "message": {
+                id: 1,
+            }
+        }
+    });
+}
+);
 
 
+//DELETE: /api/v1/messages/:id
+//verwijder één specifiek bericht uit je array (gebruik array key 0, 1, ... of fake dit)
+//geef een correct antwoord terug net zoals we dat hierboven gedaan hebben (status, message)
+//je response bevat status en message
+
+router.delete("/:id", (req, res, next) => {
+    res.status(200).json({
+        status: "success",
+        message: "DELETE message"
+    });
+}
+);
 module.exports = router;
-
